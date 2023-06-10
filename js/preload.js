a3fce317d0bf2a2121ce0c9d7fbc9f17f3f5534c
Patricia Atanasoff <system.informatica365@gmail.com>
2023-06-10 04:26:23 -0300

    hide whatsapp until page load

--- js/preload.js
+++ js/preload.js
@@ -1,6 +1,17 @@
-
+// Preloader
 let preload = document.getElementById('preload')
 
 function myFunction(){
     preload.style.display = 'none'
 }
+
+// Botón whatsapp
+
+window.addEventListener('load', function () {
+    // Obtiene el contenedor del botón de WhatsApp
+    let whatsappContainer = document.getElementById('button-w');
+
+    // Muestra el botón de whatsapp una vez que la página haya cargado completamente
+    whatsappContainer.style.display = 'block';
+    
+});
